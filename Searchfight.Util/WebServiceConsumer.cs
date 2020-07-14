f816f573342486ed48ea920548a2e37af4ad36a5
Diego Torres <diegot1106@gmail.com>
2020-07-14 10:54:51 -0500

    Singleton, thread-safe HttpClient

--- Searchfight.Util/WebServiceConsumer.cs
+++ Searchfight.Util/WebServiceConsumer.cs
@@ -8,6 +8,28 @@ namespace Util
 {
     public class WebServiceConsumer
     {
+        private static volatile HttpClient _client;
+        private static object _locker = new object();
+
+        private static HttpClient Client
+        {
+            get
+            {
+                if (_client == null)
+                {
+                    lock (_locker)
+                    {
+                        if (_client == null)
+                        {
+                            _client = new HttpClient();
+                        }
+                    }
+                }
+
+                return _client;
+            }
+        }
+
         public static async Task<T> Consume<T>(string url, IDictionary<string, object> headers = null)
         {
             return await ConsumeAsync<T>(new Uri(url), headers);
@@ -15,20 +37,25 @@ namespace Util
 
         public static async Task<T> ConsumeAsync<T>(Uri url, IDictionary<string, object> headers = null)
         {
-            var client = new HttpClient();
+            ClearHeaders();
 
             if (headers != null)
             {
                 foreach (var header in headers)
                 {
-                    client.DefaultRequestHeaders.Add(header.Key, header.Value.ToString());
+                    Client.DefaultRequestHeaders.Add(header.Key, header.Value.ToString());
                 }
             }
 
-            string resultString = await client.GetStringAsync(url);
+            string resultString = await Client.GetStringAsync(url);
             T result = JsonConvert.DeserializeObject<T>(resultString);
 
             return result;
+        }
+
+        private static void ClearHeaders()
+        {
+            Client.DefaultRequestHeaders.Clear();
         }
     }
 }
